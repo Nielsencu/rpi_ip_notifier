92b9d6aa6a3b440a2f995c92f1ac77772b6e452a
Nielsencu <nielsencugito@gmail.com>
2022-12-01 18:28:59 +0000

    Added hostname to message

---
+++ send_ip.py
@@ -0,0 +1,24 @@
+import time
+import requests
+import subprocess
+from datetime import datetime
+from config import BOT_TOKEN, BOT_CHATID
+
+def sendIp(hostname : str, ip : str):
+    now = datetime.now()
+    API_HEADER = 'https://api.telegram.org/bot' + BOT_TOKEN + '/sendMessage?chat_id=' + BOT_CHATID + '&parse_mode=Markdown&text='
+    msg = API_HEADER + f'Raspberry Pi {hostname} alive!\nIP: {ip}\n' + f'Date: {now.strftime("%d/%m/%Y %H:%M:%S")}'
+    response = requests.get(msg)
+
+    return response.json()
+
+ip = subprocess.run(['hostname', '-I'], stdout=subprocess.PIPE).stdout.decode('utf-8').rstrip('\n')
+hostname = subprocess.run(['hostname'], stdout=subprocess.PIPE).stdout.decode('utf-8').rstrip('\n')
+sent = False
+while not sent:
+    try:
+        sendIp(hostname, ip)
+        sent = True
+    except:
+        time.sleep(2)
+        continue
